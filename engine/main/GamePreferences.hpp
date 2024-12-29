cde3b4aa7da451db76c29ec3ad76d973b26906de
vgglv <ripykt@gmail.com>
2024-12-29 19:05:29 +0900

    Window opening

--- engine/main/GamePreferences.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace GameEngine {
-    struct GamePreferences {
-        std::string name;
-    };
-}
--- galaga/src/main.cpp
+++ galaga/src/main.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
-#include "main/GamePreferences.hpp"
+#include <cstdlib>
+#include "engine/Engine.hpp"
 
 int main() {
-    std::cout << "Hello, world!\n";
-    GameEngine::GamePreferences preferences = {"Galaga"};
-    return 0;
+	GameEngine::Engine engine;
+	engine.setGameResolution(800, 800);
+	engine.setGameTitle("Galaga");
+	if (!engine.startMainLoop()) {
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
